c2e504ae8d3c0243869b8317babc34e4ba522b07
Sangamesh <sangabi1@in.ibm.com>
2019-07-17 16:24:57 +0530

    Reagistration updated

--- HomePage/src.js
+++ HomePage/src.js
@@ -1,3 +1,79 @@
-const callReg=()=>{
     
-}
+$(document).ready(()=> 
+{
+            let lastpass;
+    
+        $('#log-in').hide();
+        $('#reg-field').hide();
+        console.log("inside ready function ")
+        $.validator.addMethod('emailValidator',(val)=>{
+            let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return reg.test(val);
+        })
+        $.validator.addMethod("phoneValidator",(val)=>{
+            if(val>6000000000 && val <9999999999)
+             return true;
+        })
+        $.validator.addMethod('passValidator',(val)=>{
+            // console.log(val);
+            lastpass=val;
+           let reg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+           return reg.test(val);
+        })
+        $.validator.addMethod('repassValidator',(val)=>{
+            console.log("in recheck",lastpass);
+            if(lastpass==val){
+                return true;
+            }
+        })
+        $('#logIn-btn a').click(()=>{
+        // $('#log-in').show()
+        $('#log-in').slideToggle(2000)
+     });
+  
+       $('#reg-btn').click(()=>{
+        // alert('hello');
+        $('#log-in').fadeOut(500);
+         $('#reg-field').fadeIn(2000);
+       })
+
+
+       $('#re-sub').click(()=>{
+           $('#reg_form').validate({
+               rules:{
+                reg_name:'required',
+                reg_dob:'required',
+                reg_email:{
+                    required:true,
+                    emailValidator:true
+                },
+                reg_password:{
+                    required:true,
+                    passValidator:true
+                },
+                reg_repassword:{
+                    required:true,
+                    repassValidator:true
+                },
+                reg_phone:{
+                    required:true,
+                    phoneValidator:true
+                }
+               },
+               messages:{
+                   reg_email:'Not a Valid Email',
+                   reg_password:'Not a Strong Password',
+                   reg_repassword:'Not same ',
+                   reg_phone:'please enter a valid phone'
+               }
+           })
+       })
+
+    });
+
+
+
+
+
+
+
