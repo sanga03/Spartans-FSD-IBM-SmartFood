a452906129b9ef09eb7299d3e1675402fcf6ff53
b4ibmjava21@iiht.tech <b4ibmjava21@iiht.tech>
2019-07-26 15:38:22 +0530

    cards UI

---
+++ Restaurants_And_Foods_Display_Page/restaurantPage.js
@@ -0,0 +1,8 @@
+const description1 = ()=>{
+    document.getElementById("test").hidden = false;
+    console.log("Changing");
+}
+const description2 = ()=>{
+    document.getElementById("test").hidden = true;
+    console.log("Changing");
+}
