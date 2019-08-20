427abb9b3ebd060eae9f1e80f3ce44d00d2adbef
Sangamesh <sangabi1@in.ibm.com>
2019-08-20 23:03:34 +0530

    Angular Updates

--- AngularFEHOME/src/app/anim-slider/anim-slider.component.ts
+++ AngularFEHOME/src/app/anim-slider/anim-slider.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbdCarouselBasic} from 'bootstrap'
 @Component({
   selector: 'app-anim-slider',
   templateUrl: './anim-slider.component.html',
   styleUrls: ['./anim-slider.component.css']
 })
 export class AnimSliderComponent implements OnInit {
-
-  constructor() { }
+images=[
+  'https://www.wineanddine.com.sg/wp/wp-content/uploads/2017/05/Photo-Burger-Lobster-Grilled-Lobster-Lobster-Rolls-Burger.jpg',
+  'https://spanishsabores.com/wp-content/uploads/2019/01/DSC07278.jpg',
+  'http://www.thenutfit.com/pro_images/calories13_07_08.png']
+   constructor() { }
 
   ngOnInit() {
   }
--- AngularFEHOME/src/app/app.module.ts
+++ AngularFEHOME/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -20,6 +20,7 @@ import { SignComponent } from './sign/sign.component';
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
