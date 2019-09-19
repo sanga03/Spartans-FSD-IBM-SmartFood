1ab6f06c05053b2438ea3771f864f695ad5bc49b
sangamesh <b4ibmjava20@iiht.tech>
2019-09-19 16:58:55 +0530

    UserPreference

---
+++ MicroServices/UserPreferenceMicroService/src/main/java/com/example/demo/UserPreferenceMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPreferenceMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPreferenceMicroServiceApplication.class, args);
+	}
+
+}
