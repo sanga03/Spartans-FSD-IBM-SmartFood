bfe57df6e22e8cbb848d5069dc33868ee1872915
sangamesh <b4ibmjava20@iiht.tech>
2019-09-24 14:26:49 +0530

    Dir Refactor

---
+++ IInd Layer Services/CustomerPreference/src/main/java/com/example/demo/CustomerPreferenceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerPreferenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerPreferenceApplication.class, args);
+	}
+
+}
