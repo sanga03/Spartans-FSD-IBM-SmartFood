c1dc636f44117e2cb4f2a70748fc716a042298df
b4ibmjava21@iiht.tech <b4ibmjava21@iiht.tech>
2019-10-09 11:06:26 +0530

    updated backup

--- MicroServices/Microservice_SmartFood_CustomIngredient/src/test/java/com/base_package/MicroserviceSmartFoodCustomIngredientApplicationTests.java
+++ MicroServices/Microservice_SmartFood_CustomIngredient/src/test/java/com/base_package/MicroserviceSmartFoodCustomIngredientApplicationTests.java
@@ -5,16 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.base_package.service.CustomIngredientService;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MicroserviceSmartFoodCustomIngredientApplicationTests {
 
 	@Test
 	public void testCustomIngredientService() {
-		CustomIngredientService customIngredientService = new CustomIngredientService();
-		
 	}
 
 }
